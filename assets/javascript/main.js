064df7eb8636b5faeebbc124e63621a1632b8d22
kanikas01 <larvakurt@yahoo.com>
2019-01-30 09:54:58 -0800

    Add basic messaging functionality

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,15 +7,36 @@ var config = {
   storageBucket: "rps-multiplayer-3c61a.appspot.com",
   messagingSenderId: "704316579170"
 };
-
 firebase.initializeApp(config);
+
 var database = firebase.database();
+
+// Messaging
+const messaging = firebase.messaging();
+
+// Add the public key generated from the console here.
+messaging.usePublicVapidKey("BBIZXWXMFrBjvjNyOwTl_RRCZEEIiy2kH-yte8-8A7FdlJbZn_nKYTvEl2mheum94umPwdp7zpbIFByKzTjhnvY");
+
+messaging.requestPermission()
+.then(function() {
+  console.log('Have permission');
+  return messaging.getToken();
+})
+.then(function(token) {
+  console.log(token);
+})
+.catch(function(err) {
+  console.log('Error occurred');
+})
+
+messaging.onMessage(function(payload) {
+  console.log('onMessage: ', payload);
+});
 
 // Selectors
 var selectors = {};
 
 // Classes
-var classes = {};
 
 // Events
 
